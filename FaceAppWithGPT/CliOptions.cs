45e011e5a987adb727bd77fbff4027802e43af1b
Markus Heid <heid.markus@web.de>
2024-03-29 22:19:25 +0100

    * Implementation of CliOptions * Implementation of CliParser * Implementation of Unit Tests

---
+++ FaceAppWithGPT/CliOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace FaceAppWithGPT
+{
+    public class CliOptions
+    {
+        [Option('s', "source-dir", Required = true, HelpText = "Specify the source directory containing images.")]
+        public string SourceDirectory { get; set; }
+
+        [Option('r', "resize", Required = false, HelpText = "Specify the size to which images should be resized.")]
+        public string Resize { get; set; } = "default";
+
+        [Option('a', "align", Required = false, HelpText = "Specify the reference image for alignment.")]
+        public string ReferenceImage { get; set; }
+
+        [Option('o', "output-dir", Required = true, HelpText = "Specify the output directory for processed images.")]
+        public string OutputDirectory { get; set; }
+
+        [Option('f', "fps", Required = true, HelpText = "Specify the frames per second for the video.")]
+        public int FramesPerSecond { get; set; }
+
+        [Option('n', "output-name", Required = true, HelpText = "Specify the name of the output video file.")]
+        public string OutputName { get; set; }
+
+        // Additional CLI options can be defined here.
+    }
+}
--- FaceAppWithGPT/Program.cs
+++ FaceAppWithGPT/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FaceAppWithGPT;
+
+try
+{
+    var options = CliParser.ParseArguments(args);
+    if (options != null)
+    {
+        // Proceed with the application flow using parsed options
+        Console.WriteLine("Command-line options parsed successfully.");
+        // Implementation for using options in the application's workflow
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    // In a real-world scenario, display help text here
+}
