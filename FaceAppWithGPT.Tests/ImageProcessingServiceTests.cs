ddaba200d4f5ea29543cb5be43259e9d5d32eb79
Markus Heid <heid.markus@web.de>
2024-05-11 22:23:33 +0200

    Prompts and Code for Interlude "Full Aligment Implementation" - Part 1 -Contains Error

---
+++ FaceAppWithGPT.Tests/ImageProcessingServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceAppWithGPT.Tests
+{
+    [TestClass]
+    public class ImageProcessingServiceTests
+    {
+        [TestMethod]
+        public async Task Given_ValidInput_When_AlignImagesAsync_Then_CompleteWithoutError()
+        {
+            // Given
+            var fileSystemMock = new Mock<IFileSystem>();
+            var service = new ImageProcessingService(fileSystemMock.Object);
+
+            // When
+            await service.AlignImagesAsync("source", "target", "reference.jpg");
+
+            // Then
+            // Assert that files are processed, logged, etc.
+        }
+    }
+}
